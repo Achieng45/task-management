7c1580f43e4c3bfc9a5e39c8a475ec64ba111c77
Stephanie Otieno <stephanie.otieno@smart.local>
2024-04-30 10:27:37 +0300

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { TaskEndpointsComponent } from './task-endpoints/task-endpoints.component';
 import { AnalyticsComponent } from './analytics/analytics.component';
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
- {path:'',component:TaskEndpointsComponent},
+//  {path:'',component:TaskEndpointsComponent},
    {path:'charts',component:AnalyticsComponent},
    
-   // {path:'taskbar',component:TaskbarComponent}
+   {path:'',component:AppComponent}
    
 ];
